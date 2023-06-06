2e82e21e4cb78d0fddac946e28b1907fb807a94e
Melo Guan <meloguan123@gmail.com>
2023-06-06 23:10:10 +1200

    Added async message bus method

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandsService.EventProcessing
+{
+    // TODO: Separate this into its own file
+    public enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory serviceScopeFactory, IMapper mapper)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            var eventType = DetermineEvent(message);
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("--> Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine the event type");
+                    return EventType.Undetermined;
+            }
+        }
+    }
+}
